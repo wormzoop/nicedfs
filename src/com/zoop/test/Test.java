fcd6f6b91df3e9de2e9bda12e9e74f204ed769f9
wormzoop <446706489@qq.com>
2017-12-27 23:11:17 +0800

    add

---
+++ src/com/zoop/test/Test.java
@@ -0,0 +1,12 @@
+package com.zoop.test;
+
+import com.zoop.web.FileUpload;
+
+public class Test {
+
+	public static void main(String[]args) {
+		FileUpload upload = new FileUpload();
+		upload.accept();
+	}
+	
+}
--- src/com/zoop/web/FileUpload.java
+++ src/com/zoop/web/FileUpload.java
@@ -1,6 +1,10 @@
 package com.zoop.web;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -17,7 +21,7 @@ public class FileUpload {
 	public void accept() {
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket();
+			serverSocket = new ServerSocket(8889);
 			System.out.println("socket monitor start");
 			ExecutorService cacheThreadPool = Executors.newCachedThreadPool();
 			while(true) {
@@ -51,6 +55,30 @@ public class FileUpload {
 			InputStream in = null;
 			try {
 				in = socket.getInputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//				String boundary = reader.readLine();
+//				String disposition = reader.readLine();
+//				int fileIndex = disposition.indexOf("filename=\"");
+//				String fileName = disposition.substring(fileIndex,disposition.indexOf("\"",fileIndex));
+//				reader.readLine();
+//				reader.readLine();
+//				//开始读取正文
+//				File file = new File("F:/upload/"+fileName);
+//				file.createNewFile();
+//				FileOutputStream out = new FileOutputStream(file);
+//				String line;
+//				while((line = reader.readLine()) != null) {
+//					if(line.startsWith(boundary) && line.endsWith("--"))
+//						break;
+//					out.write(line.getBytes());
+//					out.flush();
+//				}
+//				//读取正文结束
+//				out.close();
+				String line;
+				while((line = reader.readLine()) != null) {
+					System.out.println(line);
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}finally {
