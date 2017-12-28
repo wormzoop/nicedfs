c9cffae7ba02d374b311095947ca79ff08567b05
wormzoop <446706489@qq.com>
2017-12-28 16:14:04 +0800

    1

--- src/com/zoop/Main.java
+++ src/com/zoop/Main.java
@@ -8,8 +8,12 @@ public class Main {
 	//启动
 	public static void main(String[]args) {
 		//上传文件监听启动
-		FileUpload upload = new FileUpload();
-		upload.accept();
+		new Thread(new Runnable() {
+			public void run() {
+				FileUpload upload = new FileUpload();
+				upload.accept();
+			}
+		}).start();
 		//web服务器启动
 		new Thread(new Runnable() {
 			public void run() {
